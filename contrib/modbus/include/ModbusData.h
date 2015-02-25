78312ede9ee242dfc5d39b31d1f5b256afd2a3d6
Bruno Luiz <contato@brunoluiz.net>
2015-02-25 16:09:00 -0300

    New ModbusData structure (inherit from ModbusSlave)

--- contrib/modbus/include/ModbusData.h
+++ contrib/modbus/include/ModbusData.h
@@ -1,7 +1,7 @@
 #ifndef MODBUS_DATA_RESPONSE_H_
 #define MODBUS_DATA_RESPONSE_H_
 
-#include "PlataformSettings.h"
+#include "ModbusSettings.h"
 
 typedef struct ModbusData ModbusData;
 
@@ -9,7 +9,7 @@ struct ModbusData {
 	Uint16 slaveAddress;
 	Uint16 functionCode;
 	Uint16 contentIdx;
-	Uint16 content[16];
+	Uint16 content[MB_BUFFER_SIZE];
 	Uint16 size;
 	Uint16 crc;
 
@@ -18,9 +18,9 @@ struct ModbusData {
 	Uint16 * (*getTransmitStringWithoutCRC)(ModbusData *self);
 };
 
-inline void response_clear(ModbusData *self);
-inline Uint16 * response_getTransmitString(ModbusData *self);
-inline Uint16 * response_getTransmitStringWithoutCRC(ModbusData *self);
+inline void data_clear(ModbusData *self);
+inline Uint16 * data_getTransmitString(ModbusData *self);
+inline Uint16 * data_getTransmitStringWithoutCRC(ModbusData *self);
 ModbusData construct_ModbusData();
 
 #endif
